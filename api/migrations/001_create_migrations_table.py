86e28e24694a8b06f2864eab72c6c9c43ec1ba25
Andrew Cadena Ponce <itscadena123@gmail.com>
2023-11-15 23:16:14 +0000

    User migration table created

---
+++ api/migrations/001_create_migrations_table.py
@@ -0,0 +1,19 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY NOT NULL,
+            username VARCHAR(100) NOT NULL UNIQUE,
+            hashed_password VARCHAR(200) NOT NULL,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """
+    ],
+]
