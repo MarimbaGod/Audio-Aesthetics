ed8f4581b9b5db3aa4694068a21329e9418d094e
Gordon Tran <itsnotgordon@gmail.com>
2023-11-30 00:12:01 +0000

    Home Page

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Construct from "./Construct.js";
 // import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import Login from "./Login"
+import Homepage from "./Homepage"
 import Dashboard from "./Dashboard"
 
 const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header>
-        <h1>Audio Aesthetics</h1>
-      </header>
       <main>
-        <Login />
+        <Homepage />
+        {/* <Login /> */}
         {/* Other Components */}
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/VerticalNav.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import PublicIcon from '@mui/icons-material/Public';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import MessageIcon from '@mui/icons-material/Message';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import TuneIcon from '@mui/icons-material/Tune';
+
+import ListSubheader from '@mui/material/ListSubheader';
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <SearchIcon />
+      </ListItemIcon>
+      <ListItemText primary="Search" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PublicIcon />
+      </ListItemIcon>
+      <ListItemText primary="Explore" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AddBoxIcon />
+      </ListItemIcon>
+      <ListItemText primary="Create" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <MessageIcon />
+      </ListItemIcon>
+      <ListItemText primary="Messages" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div">
+      Accounts and Settings
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <TuneIcon />
+      </ListItemIcon>
+      <ListItemText primary="Settings" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AccountCircleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Account" />
+    </ListItemButton>
+  </React.Fragment>
+);
