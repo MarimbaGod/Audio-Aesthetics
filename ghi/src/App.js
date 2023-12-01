6a88bf58d2413c16bba082de5c54f6cc9dff0db0
Angel Lawler Skye <angellawlerskye@gmail.com>
2023-12-01 00:36:36 +0000

    Resolve "Create a sign up page using react front end"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Homepage from "./Homepage"
+import Homepage from "./Homepage";
 import SignIn from "./SignIn";
 import Logout from "./Logout";
+
+import SignupForm from "./SignupForm";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
-
 function App() {
-
   const baseUrl = "http://localhost:8000";
 
   return (
-   <BrowserRouter>
-        <AuthProvider baseUrl={baseUrl}>
-          <Routes>
-            <Route exact path="/" element={<Homepage />}></Route>
-            <Route exact path="/signin" element={<SignIn />}></Route>
-            <Route exact path="/logout" element={<Logout />}></Route>
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <AuthProvider baseUrl={baseUrl}>
+        <Routes>
+          <Route exact path="/" element={<Homepage />}></Route>
+          <Route exact path="/signin" element={<SignIn />}></Route>
+          <Route exact path="/logout" element={<Logout />}></Route>
+          <Route exact path="/signup" element={<SignupForm />}></Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/SignupForm.js
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+const SignupForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [img_url, setImageUrl] = useState("");
+  const { register } = useToken();
+  const navigate = useNavigate();
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    const userData = {
+      username: username,
+      password: password,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      img_url: img_url,
+    };
+    register(userData, `http://localhost:8000/api/users`, e.target.reset());
+    navigate("/");
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={(e) => handleRegistration(e)}
+          sx={{ mt: 3 }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="username"
+                autoComplete="username"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="password"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="first_name"
+                autoComplete="given-name"
+                required
+                fullWidth
+                id="first_name"
+                label="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="last_name"
+                required
+                fullWidth
+                id="last_name"
+                label="Last Name"
+                autoComplete="family-name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="email"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="img_url"
+                fullWidth
+                id="img_url"
+                label="Profile Pic URL"
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/signin" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default SignupForm;
