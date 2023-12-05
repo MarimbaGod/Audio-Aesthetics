2ccf8589e4a4d528f0bc944e6d8db25f79495507
Angel Lawler Skye <angellawlerskye@gmail.com>
2023-12-05 22:40:08 +0000

    Resolve "a few styling updates on signup page"

--- ghi/src/SignupForm.js
+++ ghi/src/SignupForm.js
@@ -13,6 +13,27 @@ import Box from "@mui/material/Box";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://gitlab.com/team-tunity/audio-aesthetics"
+      >
+        Audio Aesthetics
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
 const SignupForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -23,7 +44,7 @@ const SignupForm = () => {
   const { register } = useToken();
   const navigate = useNavigate();
 
-  const handleRegistration = (e) => {
+  const handleRegistration = async (e) => {
     e.preventDefault();
     const userData = {
       username: username,
@@ -33,121 +54,150 @@ const SignupForm = () => {
       email: email,
       img_url: img_url,
     };
-    register(userData, `http://localhost:8000/api/users`, e.target.reset());
-    navigate("/explore");
+
+    try {
+      await register(userData, `http://localhost:8000/api/users`);
+      e.target.reset(); // Reset the form only if the registration is successful
+      navigate("/explore");
+    } catch (error) {
+      console.error("Registration failed:", error);
+      // Handle the error (e.g., display an error message to the user)
+    }
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+    <Box
+      sx={{
+        backgroundImage: 'url("https://source.unsplash.com/random?music")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{ backgroundColor: "white", padding: 4, borderRadius: 8 }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
         <Box
-          component="form"
-          onSubmit={(e) => handleRegistration(e)}
-          sx={{ mt: 3 }}
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="username"
-                autoComplete="username"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="password"
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="first_name"
-                autoComplete="given-name"
-                required
-                fullWidth
-                id="first_name"
-                label="First Name"
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="last_name"
-                required
-                fullWidth
-                id="last_name"
-                label="Last Name"
-                autoComplete="family-name"
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="email"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                autoComplete="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="img_url"
-                fullWidth
-                id="img_url"
-                label="Profile Pic URL"
-                onChange={(e) => setImageUrl(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={(e) => handleRegistration(e)}
+            sx={{ mt: 3 }}
           >
-            Sign Up
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="/signin" variant="body2">
-                Already have an account? Sign in
-              </Link>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="username"
+                  autoComplete="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="password"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="first_name"
+                  autoComplete="given-name"
+                  required
+                  fullWidth
+                  id="first_name"
+                  label="First Name"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="last_name"
+                  required
+                  fullWidth
+                  id="last_name"
+                  label="Last Name"
+                  autoComplete="family-name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="img_url"
+                  fullWidth
+                  id="img_url"
+                  label="Profile Pic URL"
+                  onChange={(e) => setImageUrl(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+              </Grid>
             </Grid>
-          </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                backgroundColor: "common.black",
+                color: "white",
+              }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/signin" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
