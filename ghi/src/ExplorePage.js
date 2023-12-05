c58d5824c91a4a07e0446d6b4d40b7811ab5ef43
Gordon Tran <itsnotgordon@gmail.com>
2023-12-04 19:58:06 -0800

    View function works on explore page

--- ghi/src/ExplorePage.js
+++ ghi/src/ExplorePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 //actually useful react imports
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 //navbar buttons
 import MenuIcon from '@mui/icons-material/Menu';
@@ -36,6 +37,8 @@ import CardMedia from '@mui/material/CardMedia';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import Avatar from '@mui/material/Avatar';
+
+import PostDetails from './PostDetails';
 
 const defaultTheme = createTheme({
   palette: {
@@ -126,149 +129,181 @@ export default function ExplorePage() {
     fetchData();
   }, []);
 
-return (
-  <ThemeProvider theme={defaultTheme}>
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
+    const handleLike = async (postId) => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/posts/${postId}/like`, {
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);  // Log the response (you can handle it as needed)
+        // Add logic to update the UI or perform any other actions
+      } else {
+        const errorData = await response.json();
+        console.error('Failed to like the post:', errorData.message);
+        // Handle the error, display a message, etc.
+      }
+    } catch (error) {
+      console.error('Error while liking the post:', error);
+      // Handle the error, display a message, etc.
+    }
+  };
+
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const handleView = (post) => {
+    setSelectedPost(post);
+  };
+  const handleCloseModal = () => {
+    setSelectedPost(null);
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+            <Toolbar
               sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
+                pr: '24px', // keep right padding when drawer closed
               }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h4"
-              color="inherit"
-              noWrap
-              sx={{
-                flexGrow: 1,
-                display: 'flex',
-                alightItems: 'center',
-                justifyContent:'center',
-              }}
-            >
-              Audio Aesthetics
-            </Typography>
-            <IconButton color="inherit" component={Link} to="/logout">
-                <LogoutIcon />
-            </IconButton>
-          </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <Typography
-                variant="h6"
-                component="div"
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
                 sx={{
+                  marginRight: '36px',
+                  ...(open && { display: 'none' }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h4"
+                color="inherit"
+                noWrap
+                sx={{
+                  flexGrow: 1,
                   display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  height: '20px', // Adjust the height as needed
-                  fontWeight: 'bold', // You can adjust the styling as needed
+                  alightItems: 'center',
+                  justifyContent:'center',
                 }}
               >
                 Audio Aesthetics
               </Typography>
-            <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto',
-        }}
-      >
-      <Toolbar />
-      {/* This holds the grid system  */}
-      <Container sx={{ py: 8, mt: 4}} maxWidth="md">
-        <Grid container spacing={3}>
-          {posts.map((post) => (
-          <Grid item key={post.id} xs={12} sm={6} md={4}>
-            <Card
+              <IconButton color="inherit" component={Link} to="/logout">
+                  <LogoutIcon />
+              </IconButton>
+            </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+            <Toolbar
               sx={{
-                height: '100%',
                 display: 'flex',
-                flexDirection: 'column',
-                maxWidth: '500px',
-                margin: 'auto',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1],
               }}
             >
-              {users.find((user) => user.id === post.created_by) && (
-                <Stack direction="row" alignItems="center" spacing={1} >
-                  <Avatar
-                    src={users.find((user) => user.id === post.created_by).img_url}
-                    alt="Profile"
-                    />
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: 'bold',
-                      fontSize: '1.2rem',
-                    }}
-                  >
-                    {users.find((user) => user.id === post.created_by).username}
-                  </Typography>
-                </Stack>
-              )}
-              {post.img_url && (
-                <CardMedia
+              <IconButton onClick={toggleDrawer}>
+                <Typography
+                  variant="h6"
                   component="div"
                   sx={{
-                    // 16:9
-                    pt: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '20px', // Adjust the height as needed
+                    fontWeight: 'bold', // You can adjust the styling as needed
                   }}
-                  image={post.img_url}
-                />
-              )}
-              <CardActions>
-                <Button size="small">
-                  <FavoriteBorderIcon />
-                </Button>
-                <Button size="small">
-                  View
-                </Button>
-              </CardActions>
-            </Card>
+                >
+                  Audio Aesthetics
+                </Typography>
+              <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              {mainListItems}
+              <Divider sx={{ my: 1 }} />
+              {secondaryListItems}
+            </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+        <Toolbar />
+        {/* This holds the grid system  */}
+        <Container sx={{ py: 8, mt: 4}} maxWidth="md">
+          <Grid container spacing={3}>
+            {posts.map((post) => (
+            <Grid item key={post.id} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '500px',
+                  margin: 'auto',
+                }}
+              >
+                {users.find((user) => user.id === post.created_by) && (
+                  <Stack direction="row" alignItems="center" spacing={1} >
+                    <Avatar
+                      src={users.find((user) => user.id === post.created_by).img_url}
+                      alt="Profile"
+                      />
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        fontWeight: 'bold',
+                        fontSize: '1.2rem',
+                      }}
+                    >
+                      {users.find((user) => user.id === post.created_by).username}
+                    </Typography>
+                  </Stack>
+                )}
+                {post.img_url && (
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '100%',
+                    }}
+                    image={post.img_url}
+                  />
+                )}
+                <CardActions>
+                  <Button size="small">
+                    <FavoriteBorderIcon onClick={() => handleLike(post.id)}/>
+                  </Button>
+                  <Button size="small" onClick={() => handleView(post)}>
+                    View
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
           </Grid>
-        ))}
-        </Grid>
-      </Container>
+        </Container>
+        <PostDetails post={selectedPost} users={users} open={selectedPost !== null} onClose={handleCloseModal} />
+        </Box>
       </Box>
-    </Box>
-  </ThemeProvider>
-  );
+    </ThemeProvider>
+
+    );
 }
