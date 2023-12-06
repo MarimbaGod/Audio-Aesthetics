ae7bbc5bcba2c87f47f2bf3775ff66f75d6db9cc
Kyle Bossert <bossertkt@gmail.com>
2023-12-06 03:52:22 +0000

    Resolve "Unittest for get_all_users"

--- api/tests/test_get_users.py
+++ api/tests/test_get_users.py
@@ -1,2 +1,59 @@
-def test_init():
-    assert 1 == 1
+from unittest import TestCase
+from fastapi import status
+from fastapi.testclient import TestClient
+from queries.users import UserRepository
+from main import app
+
+
+client = TestClient(app)
+
+
+class TestUserQueries(TestCase):
+    def get_all(self):
+        return [
+            {
+                "id": 1,
+                "username": "ifkylethenkyle",
+                "first_name": "Kyle",
+                "last_name": "Kyle",
+                "email": "kyle@kyle.com",
+                "img_url": "https://tinyurl.com/Dimg-url"
+            },
+            {
+                "id": 2,
+                "username": "itsnotgordon",
+                "first_name": "Gordon",
+                "last_name": "Ramsay",
+                "email": "wonderfulman@food.com",
+                "img_url": "https://tinyurl.com/Dimg-url"
+            }
+        ]
+
+
+def test_get_all():
+    app.dependency_overrides[UserRepository] = TestUserQueries
+    expected = [
+        {
+            "id": 1,
+            "username": "ifkylethenkyle",
+            "first_name": "Kyle",
+            "last_name": "Kyle",
+            "email": "kyle@kyle.com",
+            "img_url": "https://tinyurl.com/Dimg-url"
+        },
+        {
+            "id": 2,
+            "username": "itsnotgordon",
+            "first_name": "Gordon",
+            "last_name": "Ramsay",
+            "email": "wonderfulman@food.com",
+            "img_url": "https://tinyurl.com/Dimg-url"
+        }
+    ]
+
+    response = client.get("/api/users/")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == expected
