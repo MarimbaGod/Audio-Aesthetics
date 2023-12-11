dc91f972ae43c6a366a7932e3de381d9d33bc3d2
Kyle <bossertkt@gmail.com>
2023-12-11 12:17:56 -0800

    removed unused

--- ghi/src/useSpotifyAuth.js
+++ ghi/src/useSpotifyAuth.js
@@ -1,9 +1,9 @@
 import {useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const useSpotifyAuth = () => {
     const [isAuthorized, setIsAuthorized] = useState(false);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     useEffect(() => {
         const code = new URLSearchParams(window.location.search).get('code');
@@ -26,7 +26,7 @@ const useSpotifyAuth = () => {
             })
             .catch(error => console.error('Error:', error));
         }
-    }, [navigate]);
+    }, []);
 
 
     return isAuthorized;
